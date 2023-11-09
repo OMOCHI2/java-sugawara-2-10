d7f969c26692b4cc6cdcb09441acffe197c21590
OMOCHI <satsukisama.mochi@gmail.com>
2023-11-10 00:24:36 +0900

    4-10問題１抽象クラスを実装

---
+++ Taxi.java
@@ -0,0 +1,8 @@
+public class Taxi extends Vehicle {
+  public Taxi(int crewNum) {
+    this.crewNum = crewNum;
+  }
+  public void showCrewNum() {
+    System.out.println("タクシーの乗客：" + crewNum + "名");
+  }
+}
